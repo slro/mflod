3f20912b3084860084c9f7cc7f6cac9481c56612
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2017-05-08 14:28:41 +0300

    Defines logging basic configuration

--- mflod/crypto/key_manager.py
+++ mflod/crypto/key_manager.py
@@ -1,3 +1,6 @@
+import logging
+
+
 class KeyManager(object):
     """
     class is responsible for managing and retrieving GPG keys from GPG keychain
@@ -14,4 +17,13 @@ class KeyManager(object):
     """
 
     def __init__(self):
-        pass
+        """
+        Initialize KeyManager class
+
+        Defines logging basic configuration
+        """
+
+        logging.basicConfig(filename='../../log/key_manager.log', level=logging.DEBUG)
+
+        self.logger = logging.getLogger(__name__)
+        self.logger.debug('KeyManager instance is being created.')
