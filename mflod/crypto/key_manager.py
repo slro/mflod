e8488677d43d736eee04db931da14b66be396bd3
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2017-05-10 21:06:06 +0300

    Defines parent child class relationship and tweaks up docstrings

--- mflod/crypto/key_manager.py
+++ mflod/crypto/key_manager.py
@@ -1,15 +1,16 @@
-import logging
+from mflod.crypto.gnupg_wrapper import GnuPGWrapper
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 
 
-class KeyManager(object):
+class KeyManager(GnuPGWrapper):
     """
-    Class manages operations on user GPG keys
+    Class manages operations on user keys, implements GnuPGWrapper parent class
 
-    Specifically, goal is to implement and provide gnupg_wrapper.py (GnuPGWrapper) functionality,
-    generate plain RSA key pair and support helper methods. Also convert and provide PGP keys to
-    a cryptography lib key instances.
+    Specifically, goal is to implement and provide parent class (GnuPGWrapper) functionality,
+    generate plain RSA key pair, parse and retrieve PGP key information such as RSA semi-primes
+    (p, q, n, e, d) in order to generate specific PGP key's RSA key. In addition convert and
+    return PGP keys to a cryptography lib object instances for further processing.
 
     Developers:
         - Tornike Nanobashvili
@@ -17,13 +18,11 @@ class KeyManager(object):
 
     def __init__(self):
         """
-        Initialize KeyManager class
-
-        Initialization processes:
-            - Defines logging instance
+        Initialize KeyManager and parent GnuPGWrapper classes
         """
 
-        self.logger = logging
+        GnuPGWrapper.__init__(self)
+
         self.logger.debug('KeyManager instance is being created.')
 
     def gen_plain_rsa_key(self, key_size=2048):
@@ -38,7 +37,7 @@ class KeyManager(object):
         :return: object
         """
         try:
-            # Generates RSA key pair
+            # Generates plain RSA key pair
             key = rsa.generate_private_key(
                 public_exponent=65537,
                 key_size=key_size,
