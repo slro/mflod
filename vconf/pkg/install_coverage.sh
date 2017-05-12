7d0c76a49795943e405abdf1c8246afe3c984cce
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2017-05-12 10:20:11 +0300

    Removes coverage.py

--- vconf/pkg/install_coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# cd to import directory
-cd /vagrant/vconf || exit 1
-source provision_helper.sh
-
-install_package 'python3 -c "import coverage"' 'coverage' "
-pip3 install coverage
-"
