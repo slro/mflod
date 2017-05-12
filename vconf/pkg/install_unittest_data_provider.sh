3b95f7f7c183d86bd1116671a6a4b98e43390847
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2017-05-12 10:12:35 +0300

    Fixes pip3 upgrade

--- vconf/pkg/install_unittest_data_provider.sh
+++ vconf/pkg/install_unittest_data_provider.sh
@@ -5,5 +5,6 @@ cd /vagrant/vconf || exit 1
 source provision_helper.sh
 
 install_package 'python3 -c "import unittest_data_provider"' 'unittest_data_provider' "
+pip3 install -U pip;
 pip3 install unittest_data_provider
 "
--- vconf/pkg/system_update.sh
+++ vconf/pkg/system_update.sh
@@ -3,5 +3,3 @@
 echo ' >> RUNNING SYSTEM UPDATE ...'
 sudo apt-get -y update > /dev/null 2>&1
 sudo apt-get -y upgrade > /dev/null 2>&1
-# Upgrade pip to newest version
-pip3 install -U pip
